27268596c1e811705ee3d1eec25cec8ac6963766
Thorsten Fischer <thorsten@froschi.org>
2012-06-11 14:28:44 +0200

    Added dependency on libkeyutils cookbook.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w/ubuntu/.each { |os| supports os }
+
+depends          "libkeyutils", ">= 0.0.1"
